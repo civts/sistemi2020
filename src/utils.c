2475fd70701e1cf357f02713073f2253c920d398
Francesco Bozzo <francesco.bozzo@studenti.unitn.it>
2020-04-30 21:14:17 +0200

    Aggiunte conversioni int<->4 byte

---
+++ src/utils.c
@@ -0,0 +1,28 @@
+#define INT_SIZE 4
+typedef unsigned int uint;
+typedef unsigned char byte;
+
+uint fromBytesToInt(byte*);
+void fromIntToBytes(uint, byte[]);
+
+// convert 4 bytes in unsigned int (little endian)
+uint fromBytesToInt(byte *bytes){
+    uint result = 0, base = 1;
+
+    for (int i=INT_SIZE-1; i>=0; i--){
+        result += bytes[i] * base;
+        base *= 256;
+    }
+
+    return result;
+}
+
+// convert an unsigned int in 4 bytes (little endian)
+void fromIntToBytes(uint value, byte out[]){
+    uint result = 0, base = 0;
+
+    for (int i=INT_SIZE-1; i>=0; i--){
+        out[i] = ((value >> base) & 0xFF);
+        base += 8;
+    }
+}
