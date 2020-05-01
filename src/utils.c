620a7d885343ea37aa726987e97b67f762120afb
Francesco Bozzo <francesco.bozzo@studenti.unitn.it>
2020-05-01 21:14:37 +0200

    Utils: evita doppio include e isDirectory

--- src/utils.c
+++ src/utils.c
@@ -1,3 +1,7 @@
+#ifndef __UTILS_H__
+#define __UTILS_H__
+
+#include <string.h>
 #define INT_SIZE 4
 #define true 1
 #define false 0
@@ -9,12 +13,14 @@ typedef int bool;
 
 uint fromBytesToInt(byte[]);
 void fromIntToBytes(uint, byte[]);
+bool isDirectory(string, char);
 
 // convert 4 bytes in unsigned int (little endian)
 uint fromBytesToInt(byte *bytes){
     uint result = 0, base = 1;
 
-    for (int i=INT_SIZE-1; i>=0; i--){
+    int i;
+    for (i=INT_SIZE-1; i>=0; i--){
         result += bytes[i] * base;
         base *= 256;
     }
@@ -25,9 +31,26 @@ uint fromBytesToInt(byte *bytes){
 // convert an unsigned int in 4 bytes (little endian)
 void fromIntToBytes(uint value, byte out[]){
     uint result = 0, base = 0;
-
-    for (int i=INT_SIZE-1; i>=0; i--){
+    int i;
+    for (i=INT_SIZE-1; i>=0; i--){
         out[i] = ((value >> base) & 0xFF);
         base += 8;
     }
 }
+
+// check is a given path is a folder or not. You need
+// to specify the endingChar (: or /).
+bool isDirectory(string path, char endingChar){
+    bool isDirectory = false;
+    int pathLen = strlen(path);
+
+    if (pathLen == 0){
+        isDirectory = false;
+    } else {
+        isDirectory = (path[pathLen-1] == endingChar);
+    }
+
+    return isDirectory;
+}
+
+#endif
