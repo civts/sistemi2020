72704c691aa97706639c88a74a7e6015f64c1f1c
Francesco Bozzo <francesco.bozzo@studenti.unitn.it>
2020-05-01 21:19:25 +0200

    Elenco dei file nel primo livello dei folder

--- src/analyzer.c
+++ src/analyzer.c
@@ -2,8 +2,9 @@
 #include <string.h>
 #include <stdlib.h>
 #include "utils.c"
+#include "crawler.c"
 
-#define MAX_FILES 10
+#define MAX_FILES 50
 
 // Usages:
 // -i: interactive mode
@@ -15,20 +16,55 @@
 // 2: n and m are not numeric non-zero values
 // 3: usage mode not supported
 
+int  modeSwitcher(int, int, int, string[], char);
 void helpMode();
 void interactiveMode();
-void staticMode(int, int, string[]);
+void staticMode(int, int, int, string[]);
+
+bool isValidMode(string);
+int getFilePathsFromArgv(string[], string[], int);
+
+// check if the mode is a two char string, with the
+// first char being a -
+bool isValidMode(string mode){
+    return (strlen(mode) == 2) && (mode[0] == '-');
+}
+
+// extract file paths from argv array. In case of folder,
+// it uses the crawler to inspect inner files and folders.
+// It returns the number of scanned files.
+int getFilePathsFromArgv(string argv[], string fileLists[], int numPaths){
+    const int padding = 4;    // index inside argv from which filenames occur
+    unsigned long numFiles=0; // number of files recognized
+
+    int i;
+    for (i=0; i<numPaths; i++){
+        if (isDirectory(argv[i+padding], '/')){
+            int outNewFiles = 0;
+            crawler(argv[i+padding], fileLists+numFiles, &outNewFiles);
+            numFiles += outNewFiles;
+        } else {
+            fileLists[numFiles] = (string) malloc(strlen(argv[i+padding]) + 1);
+            strcpy(fileLists[numFiles], argv[i+padding]);
+            numFiles++;
+        }
+    }
+
+    return (int)numFiles;
+}
 
 int main(int argc, char *argv[]){
     int returnCode = 0;
-    int numOfP = 0, numOfQ = 0;
+    int numOfP = 0, numOfQ = 0, numOfFiles = 0;
 
     // TODO implement this as a list with pointers
     string filePaths[MAX_FILES]; // list of files to scan
 
-    if (argc <= 4){
-        // TODO check if argc >= 2 and argv[1] has 2 chars
-        if (argv[1][1] == 'h'){
+    if (argc == 1){
+        printf("?Error: specify a valid mode, n, m and at least one file/folder\n");
+        returnCode = 1;
+    } else if (argc <= 4){
+        if (isValidMode(argv[1]) && argv[1][1] == 'h'){
             helpMode();
         } else {
             printf("?Error: specify a valid mode, n, m and at least one file/folder\n");
@@ -36,35 +72,50 @@ int main(int argc, char *argv[]){
         }
     } else {
         // parsing argv
-        // check if argv[1] has 2 chars
-        char mode = argv[1][1]; // -i, -s, -h
-
-        // TODO use strol/stroll for parsing integer values
-        numOfP = atoi(argv[2]);
-        numOfQ = atoi(argv[3]);
-
-        // TODO input file list
-
-        if (numOfP==0 || numOfQ==0){
-            printf("Error: specify numeric non-zero values for n and m\n");
-            returnCode = 2;
+        if (!isValidMode(argv[1])){
+            printf("?Error: specify a valid mode, n, m and at least one file/folder\n");
+            returnCode = 1;
         } else {
-            switch (mode){
-                // TODO check for help mode before num of argument:
-                case 'h':
-                    helpMode();
-                    break;
-                case 'i':
-                    interactiveMode();
-                    break;
-                case 's':
-                    staticMode(numOfP, numOfQ, filePaths);
-                    break;
-                default:
-                    printf("Error: mode non supported\n");
-                    returnCode = 3;
+            char mode = argv[1][1]; // -i, -s, -h
+
+            // TODO use strol/stroll for parsing integer values
+            numOfP = atoi(argv[2]);
+            numOfQ = atoi(argv[3]);
+
+            // get file paths with the crawler
+            numOfFiles = getFilePathsFromArgv(argv, filePaths, argc-4);
+
+            if (numOfP==0 || numOfQ==0){
+                printf("Error: specify numeric non-zero values for n and m\n");
+                returnCode = 2;
+            } else {
+                returnCode = modeSwitcher(numOfP, numOfQ, numOfFiles, filePaths, mode);
             }
         }
+    }
+
+    return returnCode;
+}
+
+// Switch mode of the analyzer (interactive, static, help)
+// Error codes:
+// 3 - mode not supported
+int modeSwitcher(int numOfP, int numOfQ, int numOfFiles, string filePaths[], char mode){
+    int returnCode = 0;
+
+    switch (mode){
+        case 'h':
+            helpMode();
+            break;
+        case 'i':
+            interactiveMode();
+            break;
+        case 's':
+            staticMode(numOfP, numOfQ, numOfFiles, filePaths);
+            break;
+        default:
+            printf("Error: mode not supported\n");
+            returnCode = 3;
     }
 
     return returnCode;
@@ -87,6 +138,8 @@ void interactiveMode(){
     printf("Interactive mode\n");
 }
 
-void staticMode(int numOfP, int numOfQ, string filePaths[]){
+void staticMode(int numOfP, int numOfQ, int numOfFiles, string listFilePaths[]){
     printf("Static mode\n");
+    printf("Files to process:\n");
+    printFileList(listFilePaths, numOfFiles);
 }
--- src/crawler.c
+++ src/crawler.c
@@ -8,45 +8,117 @@
 
 #define READ 0
 #define WRITE 1
+#define SIZE_OF_BUFFER_TO_READ_PIPE 256
 
+void printFileList(string fileList[], int numFiles){
+    int i;
+    for (i=0; i<numFiles; i++){
+        printf("File %d: %s\n", i+1, fileList[i]);
+    }
+}
 
-void crawler(string folder, string *fileList){
-    
+void addFolderToFileName(string out, string folder, string fileName){
+    strcpy(out, folder);
+    strcat(out, fileName);
+}
+
+int getOutputFromLS(int readDescriptor, string fileList[], string folder){
+    FILE *pipeToRead = fdopen(readDescriptor, "r");
+    int numOfFileNamesProcessed = 0;
+
+    // TODO: check if 256 as limit can be a problem for file names
+    string buffer = (string) malloc(SIZE_OF_BUFFER_TO_READ_PIPE);
+
+    while(fgets(buffer, SIZE_OF_BUFFER_TO_READ_PIPE, pipeToRead) != NULL){
+
+        string fileName = strtok(buffer, "\n"); // delete ending \n in filename string
+
+        // do not include subdirectories
+        if (!isDirectory(fileName, '/')){
+            fileList[numOfFileNamesProcessed] = (string) malloc(strlen(fileName) + strlen(folder) + 1);
+            addFolderToFileName(fileList[numOfFileNamesProcessed], folder, fileName);
+            // strcpy(fileList[numOfFileNamesProcessed], buffer);
+            numOfFileNamesProcessed++;
+        }
+
+    }
+    return numOfFileNamesProcessed;
+}
+
+int getOutputFromLSRec(int readDescriptor, string fileList[], string folder){
+    FILE *pipeToRead = fdopen(readDescriptor, "r");
+    int numOfFileNamesProcessed = 0;
+    // TODO: check if 256 as limit can be a problem for file names
+    string buffer = (string)malloc(SIZE_OF_BUFFER_TO_READ_PIPE);
+
+    while(fgets(buffer, SIZE_OF_BUFFER_TO_READ_PIPE, pipeToRead) != NULL){
+        string justRead = strtok(buffer, "\n");
+        // check if the string ends with ":"
+        if (isDirectory(justRead, ':')){
+            // that's a new foldah
+            printf("I found folder: %s", justRead);
+        } else {
+            // that's a file
+            // Truncate the buffer
+            string fileName = strtok(buffer, "\n");
+            // addFolderToFileName(folder, fileName);
+            fileList[numOfFileNamesProcessed] = (string) malloc(strlen(fileName) + 1);
+            strcpy(fileList[numOfFileNamesProcessed], buffer);
+            // printf("buffer: %s\n", fileName);
+            numOfFileNamesProcessed++;
+        }
+    }
+
+    return numOfFileNamesProcessed;
+}
+
+// Get all the file path inside a folder
+// Error codes:
+// 1: it was not possible to fork a child
+// 2: it was not possibile to inspect the folder
+int crawler(string folder, string fileList[], int* outNumFilesFound){
+    *outNumFilesFound = -1; // in case of error;
+    int returnCode = 0;
+    string lsArgs[] = {"ls", "-p", folder, NULL}; // test is just a placeholder because we can't pass only NULL
+    // string lsArgs[] = {"ls", "-p", "-R", NULL};
     int fds[2];
     pipe(fds);
 
-    int f;
-    f = fork();
-
-    int status = 0;
+    // TODO change type of f to pid
+    int f = fork();
     if (f < 0){
         printf("\nError creating little brittle crawler-son\n");
+        returnCode = 1;
     } else if (f == 0){
+        // child
         close(fds[READ]);
-        dup2(fds[WRITE], 1);     // substituting stdout with fds[WRITE]
-        // string args[]={"-p ", "-R", NULL};
-        string args[] ={"ciao", NULL};
-        for (long long i=0; i<9999999999; i++){}
-        // execvp("ls", args);
-        // execvp("echo", args);
-        system("echo ciao");
+        dup2(fds[WRITE], 1); // substitute stdout with fds[WRITE] for ls
+        execvp("ls", lsArgs);
+        // system("ls ../");
+        printf("Error: it is not possibile to inspect folder: %s\n", folder);
+        returnCode = 2; // should never be here if exec works fine
+    } else {
+        // parent
+        close(fds[WRITE]);
+        
+        // TODO check out == 0
+        int out; // return code from ls
+
+        // TODO change to PID
+        int pid = waitpid(f, &out, 0);
+        
+        *outNumFilesFound = getOutputFromLS(fds[READ], fileList, folder);
     }
-    // char[0] = (char*)malloc(fine-inizio+1);
-    // parent
-    printf("test\n");
-    wait(NULL);
-    close(fds[WRITE]);
-    char result[50];
-    
-    read(fds[READ], result, sizeof result);
-    result[49]='\0';
-    printf("I've found: %s\n", result);
+
+    return returnCode;
 }
 
-
-int main(){
-    string folder = "./";
-    string *fileList;
-    crawler(folder, fileList);
-    return 0;
-}
+// int main(){
+//     int numFilesFound = 0;
+//     string folder = "./";
+//     string fileList[50];
+//     crawler(folder, fileList, &numFilesFound);
+//     printf("num files2: %d\n", numFilesFound);
+//     printFileList(fileList, numFilesFound);
+//     return 0;
+// }
