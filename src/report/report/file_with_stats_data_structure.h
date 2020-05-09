533ca23edbfd9bd098f1fb011ac4cf6994bcff30
BigEmperor26 <mickyin3@gmail.com>
2020-05-09 21:56:59 +0200

    Sistemati gli errori
    Sistemati errori dovuti ad una cattiva deallocazione della memoria
    Convertiti tutti i valori in uint. Da concondare poi se sufficiente come limite massimo il tetto fornito da uint

---
+++ src/report/report/file_with_stats_data_structure.h
@@ -0,0 +1,96 @@
+#include "../utils.c"
+#include <stdio.h>  //print etc
+#include <stdlib.h> // malloc & free
+#include <string.h> //strlen e strcpy
+#ifndef FILE_WITH_STATS_DATA_STRUCTURE_H
+#define FILE_WITH_STATS_DATA_STRUCTURE_H
+
+/*  File where we define the structure of a fileWithStats and the functions that
+ * enable us to work with it. (The representation of a File and its stats)
+ *
+ * We have:
+ *  - a constructor
+ *  - a destructor
+ *  - a method for adding additional stats (that may come from other chunks of
+ *    the same file)
+*/
+
+// The representation of a file and its stats
+// Props:
+// -
+//
+// Methods for this:
+// constructorFWS       -> constructor
+// addStatsToFWS        -> add new data (from the other parts of this file)
+// deleteFWS            -> destructor
+typedef struct {
+  // path to the file
+  char *path;
+
+  // how many characters are present in this file
+  uint totalCharacters;
+
+  // array where in position i we count how many chars w/ ASCII code i are in
+  // the file
+  uint occorrenze[ASCII_LENGTH];
+
+  // if this file was analyzed because in a folder (for display purposes)
+  bool fromFolder;
+} fileWithStats;
+
+// Creates a fileWithStats and returns pointer to it - TESTED
+//
+// -------------------------------
+// TODO: ensure that **deleteFWS** frees path and fs correctly
+fileWithStats *constructorFWS(char *path, uint totalCharacters,
+                              uint occorrenze[ASCII_LENGTH], bool fromFolder) {
+  fileWithStats *fs = malloc(sizeof(fileWithStats));
+  fs->path = malloc(strlen(path));
+  strcpy(fs->path, path);
+  fs->totalCharacters = totalCharacters;
+  int i;
+  for (i = 0; i < ASCII_LENGTH; i++) {
+    fs->occorrenze[i] = occorrenze[i];
+  }
+  fs->fromFolder = fromFolder;
+  if (DEBUGGING)
+    printf("Creating a new FWS instance @%p for file w/ path %s\n", fs, path);
+  return fs;
+}
+
+// Destructor for fileWithStats
+// TODO: seems ok but we need to test for memory leaks
+void deleteFWS(fileWithStats *fs) {
+  if (DEBUGGING)
+    printf("Deleting FWS instance @%p of file w/ path %s\n", fs, fs->path);
+  free(fs->path);
+  free(fs);
+}
+
+// Adds new stats to this fileWithStats
+void addStatsToFWS(fileWithStats *fs, uint totCharsToAdd,
+                   uint occorrenze[ASCII_LENGTH]) {
+  if (DEBUGGING)
+    printf("Adding new stats to FWS object of file %s\n", fs->path);
+  fs->totalCharacters += totCharsToAdd;
+  int i;
+  for (i = 0; i < ASCII_LENGTH; i++) {
+    fs->occorrenze[i] += occorrenze[i];
+  }
+}
+
+// Prints the fileWithStats, just fore testing for now - TESTED
+void printFileWithStats(fileWithStats *fs) {
+  printf("%s\n", fs->path);
+  printf("%u\n", fs->totalCharacters);
+  // lo tolgo solo per debug più veloce
+  /*
+  printf("%d\n",fs->totalCharacters);
+    int i;
+    for(i=0;i<256;i++){
+      printf("%d\n",fs->occorrenze[i]);
+    }
+    */
+  printf("%d\n", fs->fromFolder);
+}
+#endif
---
+++ src/report/report/report.c
@@ -0,0 +1,231 @@
+#include "../packet_codes.h"
+#include "../utils.c"
+#include "./list_data_structure.h"
+#include "./report_utils.h"
+#include <fcntl.h>
+#ifndef bool
+typedef unsigned char bool;
+#define false 0
+#define true 1
+#endif
+
+// Path to the named pipe
+const char *PATH_TO_REPORT_PIPE = "/tmp/myfifo";
+// How many bytes to read every time from the pipe
+const int BATCH_SIZE = 128;
+
+void stampaGruppi(uint dati[], uint caratteriTot) {
+  char c;
+  int i = 0;
+  uint az = 0;
+  for (i = 'a'; i <= 'z'; i++) {
+    az += dati[i];
+  }
+  uint AZ = 0;
+  for (i = 'A'; i <= 'Z'; i++) {
+    AZ += dati[i];
+  }
+
+  for (i = 0; i < 256; i++) {
+    printf("%c: %u\n", c, dati[i]);
+  }
+}
+
+void stampaGruppiNonVerbosa(list *list) {
+  printf("Analyzed %d files:\n", list->count);
+  uint az, AZ, digits, spaces, punctuation, otherChars;
+  uint totalChars;
+  az = AZ = digits = spaces = punctuation = otherChars = totalChars = 0;
+  fwsNode *cursor = list->firstNode;
+  while (cursor != NULL) {
+    fileWithStats *fws = cursor->val;
+    int i;
+    int *oc = fws->occorrenze;
+    uint thisaz, thisAZ, thisDigits, thisSpaces, thisPunct;
+    thisaz = thisAZ = thisDigits = thisSpaces = thisPunct = 0;
+    for (i = 'a'; i <= 'z'; i++) {
+      thisaz += oc[i];
+    }
+    az += thisaz;
+    for (i = 'A'; i <= 'Z'; i++) {
+      thisAZ += oc[i];
+    }
+    AZ += thisAZ;
+    for (i = '0'; i <= '9'; i++) {
+      thisDigits += oc[i];
+    }
+    digits += thisDigits;
+    const char spaceChars[] = {' ', '\t', '\r', '\n', '\f', '\v'};
+    for (i = 0; i < 6; i++) {
+      thisSpaces += oc[i];
+    }
+    spaces += thisSpaces;
+    const char punctuationChars[] = {
+        ',', ';', '.', ':', '-', '?', '!', '\'', '`', '"', '*', '(', ')', '_',
+    };
+    for (i = 0; i < 14; i++) {
+      thisPunct += oc[punctuationChars[i]];
+    }
+    punctuation += thisPunct;
+    otherChars += fws->totalCharacters - thisaz - thisAZ - thisDigits -
+                  thisPunct - thisSpaces;
+    totalChars += fws->totalCharacters;
+    cursor = cursor->nextNode;
+  }
+  printf(
+      "a-z: %u\nA-Z: %u\ndigits: %u\npunctuation: %u\nother: %u\n\nTotal "
+      "charcters: %u\n",
+      az, AZ, digits, punctuation, otherChars, totalChars);
+}
+
+void stampaDefault(list *list) {
+  printf("Analyzed %d files:\n", list->count);
+  int i;
+  uint az, AZ, digits, spaces, punctuation, otherChars;
+  uint totalChars;
+  uint occCount[ASCII_LENGTH];
+  fwsNode *cursor = list->firstNode;
+  while (cursor != NULL) {
+    fileWithStats *fws = cursor->val;
+    int *oc = fws->occorrenze;
+    for (i = 0; i < ASCII_LENGTH; i++) {
+      occCount[i] += oc[i];
+    }
+    totalChars += fws->totalCharacters;
+    cursor = cursor->nextNode;
+  }
+  for (i = 0; i < ASCII_LENGTH; i++) {
+    if (i >= '!' && i < 254 && i != 127) {
+      printf("%c", i);
+    } else {
+      printf("character with extendedASCII code %d", i);
+    }
+    printf(": %u\n", occCount[i]);
+  }
+  printf("\nTotal "
+         "charcters: %u\n",
+         totalChars);
+}
+
+// Stampa su stdout le statistiche di un file
+void stampaSingoloFile(char *nomeFile, int dati[], int caratteriTot, int argc,
+                       char *argv) {
+  printf("---------------%s---------------", nomeFile);
+
+  printf("Caratteri totali: %u\n", caratteriTot);
+}
+
+// This is the function that implements report buisiness logic
+int report(int argc, const char *argv[]) {
+  int retCode = 0;
+  // This is where the state is stored: it contains the references to the
+  // "objects" representing the files and their stats.
+  list *mainList = constructorListEmpty();
+  while (1) {
+    // open the pipe
+    int fd = open(PATH_TO_REPORT_PIPE, O_RDONLY);
+    if (fd == -1) {
+      retCode = 1;
+      perror("No pipe");
+      break;
+    } else {
+      // get statusCode
+      byte statusCode;
+      read(fd, &statusCode, 1); // status code is in firts byte
+      if (statusCode == EOF) {
+        // If there is no one on the writing end of the pipe previous read will
+        // retur EOF (see
+        // https://www.geeksforgeeks.org/non-blocking-io-with-pipes-in-c/)
+        // TODO handle (no one is transmitting)
+      }
+
+      // Getting variables which I'll need anyway (path to the file and its
+      // length)
+      byte buffer[4];
+      read(fd, buffer, INT_SIZE);
+      // path length (in bytes)
+      uint pathLength = fromBytesToInt(buffer);
+      //printf("pathlength %u\n", pathLength);
+      char pathToFile[pathLength + 1];
+      read(fd, &pathToFile, pathLength + 1);
+      pathToFile[pathLength] = '\0';
+
+      if (DEBUGGING)
+        printf("Got packet w/ status code %d for file %s\n", statusCode,
+               pathToFile);
+      /*
+      int j=0;
+      for(j=0;j<pathLength+1;j++){
+        printf("%c\n",pathToFile[j]);
+      }
+      */
+      switch (statusCode) {
+      // nuovi dati per il file
+      case NEW_PACKET_CODE: {
+        //----Getting remaining data from the pipe
+
+        // Wether this file was passed directly or analyzed because it was
+        // inside of a folder
+        // è un byte, non è un bool
+        byte cameFromFolder;
+        read(fd, &cameFromFolder, 1);
+        //printf("folder %u",cameFromFolder);
+        read(fd, buffer, INT_SIZE);
+        // Total number of chars in the file
+        uint totalCharsInFile = fromBytesToInt(buffer);
+        //printf("totalCharacters %u \n",buffer[3]);
+        //printf("totalCharacters %u \n",totalCharsInFile);
+        // Array where in each position i we have the occourrences of the letter
+        // w/ ASCII code i
+        uint occourrences[ASCII_LENGTH];
+
+        int i;
+        for (i = 0; i < ASCII_LENGTH; i++) {
+          read(fd, buffer, INT_SIZE);
+          occourrences[i] = fromBytesToInt(buffer);
+        }
+
+        //----Finished getting data, now adding to mainList
+
+        fileWithStats *newFwsData = constructorFWS(
+            pathToFile, totalCharsInFile, occourrences, cameFromFolder);
+        //printFileWithStats(newFwsData);
+        int appended = updateFileData(mainList, pathToFile, newFwsData);
+        if(!appended)
+          free(newFwsData);
+        break;
+      }
+      // rimuovi questo file
+      case DELETE_FILE_CODE: {
+        removeElementByPath(mainList, pathToFile);
+        break;
+      }
+      default: {
+        printf("Huston, we have a problem: status code (packet code) %d is "
+               "unknown",
+               statusCode);
+      }
+      }
+      stampaGruppiNonVerbosa(mainList);
+    }
+  }
+  destructorList(mainList);
+  return retCode;
+}
+
+int main(int argc, const char *argv[]) {
+  int retCode = 0;
+  if (contains(argc, argv, helpFlag)) {
+    printf("this is the help text"); // TODO
+  } else {
+    // WTF ? argsAreValid mi da un bool...cosa dovrei fare?
+    bool message = argsAreValid(argc, argv);
+    if (message) {
+      // dove è report?
+      retCode = report(argc, argv);
+    }
+    // Will never have an else since when they are not valid  the program
+    // terminates
+  }
+  return retCode;
+}
