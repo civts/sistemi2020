e888c6cc0f4fb2699bc65f87c6b454e1433d5cb8
Giacomo454sq <giacozanolli@gmail.com>
2020-06-05 09:21:08 +0200

    Restore help text

--- src/report/help.h
+++ src/report/help.h
@@ -1,5 +1,30 @@
 #ifndef __HELP_H__
 #define __HELP_H__
-const char *help="Ciao, questo è il testo di help";
+const char *help =
+    "Report permette di visualizzare i risultati delle analisi \n"
+    "in maniera semplice e dinamica.\n"
+    "Nonostante sia consigliato eseguire questo programma assieme a un\n"
+    "solo analyser, anche il caso in cui ce ne siano multipli è gestito.\n"
+    "Appositi flags permettono di decidere, anche durante l'esecuzione,\n"
+    "come si vuole che vengano presentati i dati.\n"
+    "\n"
+    "Le opzioni per cambiare la visualizzazione sono le seguenti:\n"
+    "-t      Mostra i risultati dell'analisi in una tabella.\n"
+    "        è la visualizzazione di default.\n"
+    "-v      Stampa le statisiche di ogni singolo file analizzato\n"
+    "        uno dopo l'altro. (Molto verboso)\n"
+    "-c		 Mostra una visualizzazione alternativa alla tabella ma \n"
+    "        comunque relativamente compatta.\n"
+    "--only  Questo flag, seguito da una lista di percorsi permette\n"
+    "        di vedere le statistiche relative a tutti e soli i files\n"
+    "        specificati\n"
+    "\n"
+    "Di default le statistiche sono raggruppate per minuscole, maiuscole,"
+    "numeri, punteggiatura e spazi ma con il flag -e è possibile,"
+    "in tutte le visualizzazioni tranne -t, visualizzare le statistiche"
+    "dei singoli caratteri."
+    "-h		help: mostra questa schermata\n"
+    "-r		reset: cancella i dati attuali di tutte le analisi\n"
+    "-q		quit: chiude il programma\n";
 
-#endif // !1
+#endif
